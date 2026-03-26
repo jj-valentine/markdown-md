02c6eb27db56808c07efc83f42c7c6a6ce274cb4
James Valentine <j@mesvalentine.com>
2026-03-26 10:13:59 -0700

    style(toolbar): add toolbar, dropdown, and insert menu styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './styles/tokens.css'
 import './styles/reset.css'
 import './styles/layout.css'
 import './styles/editor-content.css'
+import './styles/toolbar.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
